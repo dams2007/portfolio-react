93af54204c56c3bf13a4a9fe9c179748172c410f
dams2007 <damien.bigot@gmail.com>
2018-08-02 18:22:51 +0900

    add card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/card'
 
 const IndexPage = () => (
   <div>
@@ -8,6 +9,31 @@ const IndexPage = () => (
         <h1>DESIGNER UI/UX & IOS DEVELOPER</h1>
         <p>VISUAL DESIGN, DIGITAL PRODUCTS & MOBILE APPS</p>
         <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
+    <div className="Cards">
+      <h2>Works</h2>
+      <div className="CardGroup">
+        <Card
+          title="Air Huarache"
+          description="NIKE | AIR MAX"
+          images= 'https://cl.ly/411I0E283q2p/download/air-huarache-thumbnail.png'
+        />
+        <Card
+          title="Air Huarache"
+          description="NIKE | AIR MAX"
+          images= 'https://cl.ly/411I0E283q2p/download/air-huarache-thumbnail.png'
+        />
+        <Card
+          title="Air Huarache"
+          description="NIKE | AIR MAX"
+          images= 'https://cl.ly/411I0E283q2p/download/air-huarache-thumbnail.png'
+        />
+        <Card
+          title="Air Huarache"
+          description="NIKE | AIR MAX"
+          images= 'https://cl.ly/411I0E283q2p/download/air-huarache-thumbnail.png'
+        />
       </div>
     </div>
   </div>
