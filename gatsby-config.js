c814c2493cfc435e6289645de6810a1699f3343d
dams2007 <damien.bigot@gmail.com>
2018-08-02 17:12:47 +0900

    reorganize css code

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'UX / UI Designer - Tokyo / Japan: Damien Bigot',
   },
-  plugins: ['gatsby-plugin-react-helmet'],[`gatsby-plugin-sass`],
+  plugins: ['gatsby-plugin-react-helmet','gatsby-plugin-sass']
 }
